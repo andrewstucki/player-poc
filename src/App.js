108aeae901bd320474ed0bbd90198eb3fc3e4ce5
Andrew Stucki <andrew.stucki@elastic.co>
2020-07-10 11:46:30 -0400

    Add video.js theme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import '@videojs/themes/dist/city/index.css';
+import "./App.css";
+
+class VideoPlayer extends React.Component {
+  componentDidMount() {
+    // instantiate Video.js
+    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+      console.log("onPlayerReady", this);
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+  // wrap the player in a div with a `data-vjs-player` attribute
+  // so videojs won't create additional wrapper in the DOM
+  // see https://github.com/videojs/video.js/pull/3856
+  render() {
+    return (
+      <div>
+        <div data-vjs-player>
+          <video
+            ref={(node) => (this.videoNode = node)}
+            className="video-js vjs-theme-city vjs-16-9"
+          ></video>
+        </div>
+      </div>
+    );
+  }
+}
+
+const videoJsOptions = {
+  autoplay: true,
+  controls: true,
+  sources: [
+    {
+      src:
+        "https://videodelivery.net/3d383ed53b65254ea3783cc7743702a2/manifest/video.m3u8",
+    },
+  ],
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <VideoPlayer {...videoJsOptions} />;
 }
 
 export default App;
