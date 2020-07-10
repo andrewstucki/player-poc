7239c936e4a86595ed0f4c43dd245cbb8db5ffc6
Andrew Stucki <andrew.stucki@elastic.co>
2020-07-10 11:49:08 -0400

    Remove theme package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
-import '@videojs/themes/dist/city/index.css';
 import "./App.css";
 
 class VideoPlayer extends React.Component {
